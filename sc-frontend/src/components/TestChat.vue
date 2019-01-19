612f5d96b380c249e063fad610c7ed0511995206
Poul <pashaload@gmail.com>
2019-01-19 11:53:19 +0200

    chat design

--- sc-frontend/src/components/TestChat.vue
+++ sc-frontend/src/components/TestChat.vue
@@ -197,7 +197,7 @@
   }
 
   body {
-    background: linear-gradient(135deg, #141718, #26292e);
+    background-image:url("./../assets/bg.jpg");
     background-size: cover;
     font-family: 'Open Sans', sans-serif;
     font-size: 12px;
@@ -211,8 +211,7 @@
     top: 0;
     left: 0;
     z-index: 1;
-    background: url('https://images.unsplash.com/photo-1451186859696-371d9477be93?crop=entropy&fit=crop&fm=jpg&h=975&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1925') no-repeat 0 0;
-    filter: blur(80px);
+     filter: blur(80px);
     transform: scale(1.2);
   }
 
@@ -265,7 +264,7 @@
     }
 
     .avatar {
-       position: absolute;
+      position: absolute;
       z-index: 1;
       top: 4px;
       left: 7px;
